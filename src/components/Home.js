7c74312ad1b297d6fdc0290e2c14839c1050af47
Ryan Sheehan <sheehan.w.ryan@gmail.com>
2017-10-19 01:39:44 -0400

    soon come

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,27 +5,14 @@ import { Code } from '../styles/style';
 import s from '../styles/home.style';
 
 export default function Home() {
-  const repoReadmeLink = text => (
-    <Interactive
-      as="a"
-      {...s.link}
-      href="https://github.com/rafrex/spa-github-pages#readme"
-    >{text}</Interactive>
-  );
 
   return (
     <div>
       <p style={s.p}>
-        This is an example single page app built
-        with React and React&nbsp;Router using {' '}
-        <Code>BrowserRouter</Code>. Navigate with the links below and
-        refresh the page or copy/paste the url to test out the redirect
-        functionality deployed to overcome GitHub&nbsp;Pages incompatibility
-        with single page apps (like this one).
+        First entry soon come
       </p>
       <p style={s.p}>
-        Please see the {repoReadmeLink('repo readme')} for instructions on how to
-        use this boilerplate to deploy your own single page app using GitHub Pages.
+        Founders: Ryan Sheehan & Jeffrey Bui
       </p>
       <div style={s.pageLinkContainer}>
         <Interactive
