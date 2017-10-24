c81cd00e6fd0f70b4596a9f00b76d0fbd339b027
Ryan Sheehan <sheehan.w.ryan@gmail.com>
2017-10-24 19:02:44 -0400

    images to post 1

--- src/components/Post_10_19_2017.js
+++ src/components/Post_10_19_2017.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import s from '../styles/post.style';
+import mock_one from '../images/post_one/mock_one.png'
+import mock_two from '../images/post_one/mock_two.png'
+import mock_three from '../images/post_one/mock_three.png'
+
 
 export default function Post_10_19_2017({ location }) {
   
@@ -10,6 +14,9 @@ export default function Post_10_19_2017({ location }) {
       </div>
       <div style={s.lineContainer}>
         What is good
+        <img src={mock_one} alt="mock_one"/>
+        <img src={mock_two} alt="mock_two"/>
+        <img src={mock_three} alt="mock_three"/>
       </div>
     </div>
   );
